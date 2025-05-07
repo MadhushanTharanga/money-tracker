17e098f8c7def6c7685fbeadfebee72a12594ba7
MadhushanTharanga <madhushan0617@gmail.com>
2025-05-07 22:11:21 +0530

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,26 @@ function App() {
         <button type='submit'>Add New Transaction</button>
       </form>
       <div className="transactions">
-
+        <div className="line1">
+          <div className="desc1">new bike</div>
+          <div className="money1" >-300</div>
+        </div>
+        <div className="line2">
+          <div className="desc2">test</div>
+          <div className="date-time">2025-05-07  09:27</div>
+        </div>
+        <hr />
+      </div>
+      <div className="transactions">
+        <div className="line1">
+          <div className="desc1">website gig</div>
+          <div className="money2">800</div>
+        </div>
+        <div className="line2">
+          <div className="desc2">website for my uncle</div>
+          <div className="date-time">2023-02-17  10:47</div>
+        </div>
+        <hr />
       </div>
       </div>
     </div>
